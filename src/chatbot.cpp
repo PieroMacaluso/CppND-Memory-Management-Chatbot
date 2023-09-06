62549ba3c3c81610360e949b91a313418bfeee03
Piero Macaluso <piero.macaluso@stellantis.com>
2023-09-06 14:50:21 +0200

    The rest of the tasks

--- src/chatbot.cpp
+++ src/chatbot.cpp
@@ -50,6 +50,7 @@ ChatBot::ChatBot(ChatBot &&source)
     _image = source._image;
     _chatLogic = source._chatLogic;
     _rootNode = source._rootNode;
+    _chatLogic->SetChatbotHandle(this);
     source._image = nullptr;
     source._chatLogic = nullptr;
     source._rootNode = nullptr;
@@ -66,6 +67,7 @@ ChatBot &ChatBot::operator=(ChatBot &&source)
     _image = source._image;
     _chatLogic = source._chatLogic;
     _rootNode = source._rootNode;
+    _chatLogic->SetChatbotHandle(this);
     source._image = nullptr;
     source._chatLogic = nullptr;
     source._rootNode = nullptr;
